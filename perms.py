823904dfc902f037b695ff3bb12e2cab8200a40d
Charles Reid <charlesreid1@gmail.com>
2017-12-31 16:05:15 -0800

    Add successful rotation-finding method

--- perms.py
+++ perms.py
@@ -1,7 +1,7 @@
 from rubikscubennnsolver.RubiksCube444 import RubiksCube444, solved_4x4x4
+import re
 
-
-def main():
+def test_rotations():
     # Focusing on sequences of length 3 and up
     # Focusing on right hand sequences only
     # 
@@ -15,45 +15,90 @@ def main():
 
     sequences = []
     move1 = 'U'
-    for move2 in moves:
-        for move3 in moves:
+    for move2 in ['D']:#moves:
+        for move3 in ['Uw']:#moves:
             seq = " ".join([move1,move2,move3])
             sequences.append(seq)
 
     print("Length of sequence: %d"%(len(sequences)))
 
+    for sequence in sequences:
+        print(get_rotations(sequence))
 
 
-def define_cubes():
-    ucube = ["UBFLRD",
+def get_rotations(sequence):
+    """
+    Given a cube sequence,
+    find all 24 rotations of it.
+    """
+    cubes = ["UBFLRD",
              "UFBRLD",
-             "ULRFBD"
-             "URLBFD"]
-
-    dcube = ["DFBLRU",
+             "ULRFBD",
+             "URLBFD",
+             "DFBLRU",
              "DBFRLU",
              "DLRBFU",
-             "DRLFBU"]
-
-    lcube = ["LUDBFR",
+             "DRLFBU",
+             "LUDBFR",
              "LDUFBR",
              "LFBUDR",
-             "LBFDUR"]
-
-    rcube = ["RUDFBL",
+             "LBFDUR",
+             "RUDFBL",
              "RDUBFL",
              "RBFUDL",
-             "RFBDUL"]
-
-    fcube = ["FUDLRB",
+             "RFBDUL",
+             "FUDLRB",
              "FDURLB",
              "FRLUDB",
-             "FLRDUB"]
-
-    bcube = ["BUDRLF",
+             "FLRDUB",
+             "BUDRLF",
              "BDULRF",
              "BLRUDF",
              "BRLDUF"]
+
+    results = set()
+
+    ### print("For new cube config:")
+    ### print(cubes[0])
+    ### print("Original move sequence is:")
+    ### print(sequence)
+
+    results.add(sequence)
+
+    # Here we assume first move is a U.
+    # Split the sequence into its moves,
+    # and use the first cube configuration to map
+    # moves to numbers.
+    moves = sequence.split(" ")
+    move_numbers = []
+    for move in moves:
+        move_numbers.append(cubes[0].index(move[0]))
+
+    # Now run through all other cube configurations,
+    # and map the numbers back to moves.
+    for i in range(1,len(cubes)):
+        cube = cubes[i]
+        xmoves = []
+        for i, move_number in enumerate(move_numbers):
+            old_face = cubes[0][move_number]
+            new_face = cube[move_number]
+
+            old_move = moves[i]
+            new_move = re.sub(old_face,new_face,old_move)
+
+            xmoves.append(new_move)
+
+        # Assemble the moves to a string
+        xmove = " ".join(xmoves)
+
+        ### print("For new cube config:")
+        ### print(cube)
+        ### print("New move sequence is:")
+        ### print(xmove)
+
+        results.add(xmove)
+
+    return results
 
 
 def get_cube():
@@ -62,4 +107,4 @@ def get_cube():
 
 
 if __name__=="__main__":
-    main()
+    test_rotations()
